25214f376fb8a97e42102c88b6a99791c59d9550
Leon Guo <rational_leon@163.com>
2017-03-22 07:00:26 +0800

    Revising

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get 'pages/home'
-  get "heyo" => "pages#heyo"
-
-  mount SurveyorGui::Engine => "/surveyor_gui", :as => "surveyor_gui"
-  mount Surveyor::Engine => "/surveys", :as => "surveyor"
+  get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
